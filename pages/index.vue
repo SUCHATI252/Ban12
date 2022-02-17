0944729e8bf91280bba1ed530973caa6d289e3e7
SUCHATI252 <96058817+SUCHATI252@users.noreply.github.com>
2022-02-17 21:00:17 +0900

    add demo

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,89 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+  <v-container class="fill-height justify-center">
+    <v-col cols="12" md="8">
+      <v-row justify="center" align="center">
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-card>
+            <v-card-title>
+              <span class="text-h4">วันนี้</span>
+            </v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-card dark color="green lighten-1" height="200">
+            <v-card-title class="justify-center">
+              <span class="text-h4">หายป่วยวันนี้</span>
+            </v-card-title>
+            <v-divider />
+            <v-card-text>
+              <div class="d-flex justify-center">
+                <span class="white--text text-h2 font-weight-bold">+ 000</span>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-card dark color="error" height="200">
+            <v-card-title class="justify-center">
+              <span class="text-h4">ติดเชื้อเพิ่มวันนี้</span>
+            </v-card-title>
+            <v-card-text>
+              <div class="d-flex justify-center">
+                <span class="text-h2 font-weight-bold">+ 000</span>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-card color="success" height="100">
+            sssss
+          </v-card>
+        </v-col>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-card color="error" height="100">
+            sssss
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-card color="success" height="100">
+            sssss
+          </v-card>
+        </v-col>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-card color="error" height="100">
+            sssss
+          </v-card>
+        </v-col>
+      </v-row>
     </v-col>
-  </v-row>
+  </v-container>
 </template>
 
 <script>
