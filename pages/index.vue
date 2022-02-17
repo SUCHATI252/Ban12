d0e8630c0e309fcc9e569c40623b770369c7ce5e
SUCHATI252 <96058817+SUCHATI252@users.noreply.github.com>
2022-02-17 22:21:01 +0900

    edit index

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,61 @@
 <template>
   <v-container class="fill-height justify-center">
     <v-col cols="12" md="8">
-      <v-row justify="center" align="center">
+      <v-row justify="end" align="center">
         <v-col
           cols="12"
-          md="6"
+          md="5"
         >
-          <v-card>
+          <v-card flat>
             <v-card-title>
-              <span class="text-h4">วันนี้</span>
+              <v-spacer />
+              <span>อัปเดตล่าสุด {{ getdatenow }}</span>
             </v-card-title>
           </v-card>
         </v-col>
       </v-row>
       <v-row justify="center" align="center">
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-card dark color="error" height="200">
+            <v-card-title class="justify-center">
+              <span class="text-h4">ติดเชื้อเพิ่มวันนี้</span>
+            </v-card-title>
+            <v-card-text>
+              <div class="d-flex justify-center">
+                <span class="white--text text-h2 font-weight-bold">
+                  + 1,000
+                </span>
+              </div>
+              <v-divider class="white mt-2" />
+              <div
+                class="d-flex justify-space-around pt-2"
+              >
+                <div class="d-flex flex-column">
+                  <div class="text-center">
+                    ATK
+                  </div>
+                  <v-divider />
+                  <div class="text-center">
+                    1,000
+                  </div>
+                </div>
+                <v-divider vertical class="white" />
+                <div class="d-flex flex-column">
+                  <div class="text-center">
+                    โรงพยาบาล
+                  </div>
+                  <v-divider />
+                  <div class="text-center">
+                    1,000
+                  </div>
+                </div>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
         <v-col
           cols="12"
           md="6"
@@ -25,22 +67,45 @@
             <v-divider />
             <v-card-text>
               <div class="d-flex justify-center">
-                <span class="white--text text-h2 font-weight-bold">+ 000</span>
+                <span class="white--text text-h2 font-weight-bold">
+                  1,000
+                </span>
               </div>
             </v-card-text>
           </v-card>
         </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
         <v-col
           cols="12"
           md="6"
         >
-          <v-card dark color="error" height="200">
+          <v-card dark color="error" height="120">
             <v-card-title class="justify-center">
-              <span class="text-h4">ติดเชื้อเพิ่มวันนี้</span>
+              <span>ป่วยสะสม</span>
             </v-card-title>
             <v-card-text>
               <div class="d-flex justify-center">
-                <span class="text-h2 font-weight-bold">+ 000</span>
+                <span class="white--text text-h5 font-weight-bold">
+                  0000
+                </span>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-card dark color="success" height="120">
+            <v-card-title class="justify-center">
+              <span>หายป่วยสะสม</span>
+            </v-card-title>
+            <v-card-text>
+              <div class="d-flex justify-center">
+                <span class="white--text text-h5 font-weight-bold">
+                  0000
+                </span>
               </div>
             </v-card-text>
           </v-card>
@@ -51,34 +116,34 @@
           cols="12"
           md="6"
         >
-          <v-card color="success" height="100">
-            sssss
+          <v-card dark color="cyan" height="120">
+            <v-card-title class="justify-center">
+              <span>กำลังรักษา</span>
+            </v-card-title>
+            <v-card-text>
+              <div class="d-flex justify-center">
+                <span class="white--text text-h5 font-weight-bold">
+                  0000
+                </span>
+              </div>
+            </v-card-text>
           </v-card>
         </v-col>
         <v-col
           cols="12"
           md="6"
         >
-          <v-card color="error" height="100">
-            sssss
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row justify="center" align="center">
-        <v-col
-          cols="12"
-          md="6"
-        >
-          <v-card color="success" height="100">
-            sssss
-          </v-card>
-        </v-col>
-        <v-col
-          cols="12"
-          md="6"
-        >
-          <v-card color="error" height="100">
-            sssss
+          <v-card dark color="grey" height="120">
+            <v-card-title class="justify-center">
+              <span>เสียชีวิต</span>
+            </v-card-title>
+            <v-card-text>
+              <div class="d-flex justify-center">
+                <span class="white--text text-h5 font-weight-bold">
+                  0000
+                </span>
+              </div>
+            </v-card-text>
           </v-card>
         </v-col>
       </v-row>
@@ -88,6 +153,17 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  computed: {
+    getdatenow () {
+      let today = new Date()
+      const dd = String(today.getDate()).padStart(2, '0')
+      const mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
+      const yyyy = today.getFullYear()
+
+      today = `${dd}/${mm}/${yyyy}`
+      return today
+    }
+  }
 }
 </script>
